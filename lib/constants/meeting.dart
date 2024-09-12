462ad52b870a8a996f5f52467e50da6c09b066f7
mohsen <parvar.mohsen@wallit.eu>
2024-09-13 02:48:34 +0330

    same

--- lib/constants/meeting.dart
+++ lib/constants/meeting.dart
@@ -7,9 +7,12 @@ import 'package:podium/models/user_info_model.dart';
 import 'package:podium/utils/constants.dart';
 
 class MeetingConstants {
-  static Map<String, Object?> featureFlags({
-    required bool allowedToSpeak,
-  }) {
+  static Map<String, Object?> featureFlags(
+      {required bool allowedToSpeak, required FirebaseGroup group}) {
+    final globalController = Get.find<GlobalController>();
+    final creatorId = group.creator.id;
+    final myId = globalController.currentUserInfo.value!.id;
+    final iAmCreator = creatorId == myId;
     return {
       FeatureFlags.unsafeRoomWarningEnabled: false,
       FeatureFlags.securityOptionEnabled: false,
@@ -22,9 +25,9 @@ class MeetingConstants {
       FeatureFlags.welcomePageEnabled: false,
       FeatureFlags.preJoinPageEnabled: false,
       FeatureFlags.pipEnabled: true,
-      FeatureFlags.kickOutEnabled: false,
+      FeatureFlags.kickOutEnabled: iAmCreator,
       FeatureFlags.fullScreenEnabled: true,
-      FeatureFlags.reactionsEnabled: false,
+      FeatureFlags.reactionsEnabled: true,
       FeatureFlags.videoMuteEnabled: false,
       FeatureFlags.audioMuteButtonEnabled: allowedToSpeak,
     };
@@ -56,6 +59,7 @@ class MeetingConstants {
       room: group.id,
       configOverrides: configOverrides(group),
       featureFlags: featureFlags(
+        group: group,
         allowedToSpeak: allowedToSpeak,
       ),
       userInfo: JitsiMeetUserInfo(
